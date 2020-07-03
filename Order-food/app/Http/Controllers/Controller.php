9147a0718f416a11f0e64c3d20e5dc043cf0760c
toppy368 <toppy36849@gmail.com>
2020-07-03 22:11:29 +0800

    刪除 conteroller 讓路由章節做練習

--- Order-food/app/Http/Controllers/Controller.php
+++ Order-food/app/Http/Controllers/Controller.php
@@ -7,25 +7,8 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
-//set /user routing
-use App\User;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-}
-
-//set /user Controller
-class UserController extends Controller
-{
-    /**
-     * Show the profile for the given user.
-     *
-     * @param  int  $id
-     * @return View
-     */
-    public function show($id)
-    {
-        return view('user.profile', ['user' => User::findOrFail($id)]);
-    }
 }
